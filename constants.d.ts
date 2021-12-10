488bc427f5b9277564bbcdf7f18620c5392700be
superbeanmr <beanmr888@gmail.com>
2021-12-10 16:00:53 +0800

    sdk-build

---
+++ constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x20f99E8c2583322fa3Dc3f5b00215094D8A45C3f";
+export declare const INIT_CODE_HASH = "7d9225fa97f1d72aefc85b102545d08ff9faef6b3a6583fddae791130b56f047";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
